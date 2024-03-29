62210534cedf84730e1c118969dc0127dd1e1524
Ashan <pramodya@payable.lk>
2024-03-29 14:26:35 +0530

    task 3 (insert project)

---
+++ task_3_java/src/main/java/com/dao/ProjectDAO.java
@@ -0,0 +1,66 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import com.models.Project;
+import com.DatabaseConnection.DatabaseConnection;
+
+public class ProjectDAO {
+    public void insertProject(Project project) {
+        String query = "INSERT INTO t_project (id, pro_name, status, created_at, created_by, updated_at, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, project.getId());
+            preparedStatement.setString(2, project.getProjectName());
+            preparedStatement.setBoolean(3, project.isStatus());
+            preparedStatement.setTimestamp(4, project.getCreatedAt());
+            preparedStatement.setString(5, project.getCreatedBy());
+            preparedStatement.setTimestamp(6, project.getUpdatedAt());
+            preparedStatement.setString(7, project.getUpdatedBy());
+
+            preparedStatement.executeUpdate();
+            System.out.println("Project Data insert  successfully!");
+
+        } catch (SQLException e) {
+            System.err.println("Error inserting project data: " + e.getMessage());
+        }
+    }
+    public Project loadProjectById(String id) {
+        Project project = null;
+        String query = "SELECT * FROM t_project WHERE id = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, id);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    project = new Project();
+                    project.setId(resultSet.getString("id"));
+                    project.setProjectName(resultSet.getString("pro_name"));
+                    project.setStatus(resultSet.getBoolean("status"));
+                    project.setCreatedAt(resultSet.getTimestamp("created_at"));
+                    project.setCreatedBy(resultSet.getString("created_by"));
+                    project.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+                    project.setUpdatedBy(resultSet.getString("updated_by"));
+                }
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error loading project: " + e.getMessage());
+        }
+
+        return project;
+    }
+
+
+   
+}
+
+
---
+++ task_3_java/src/main/java/com/models/Project.java
@@ -0,0 +1,59 @@
+package com.models;
+
+import java.sql.Timestamp;
+
+public class Project {
+	
+	private String id;
+	private String projectName;
+	private boolean status;
+	private Timestamp createdAt;
+	private String createdBy;
+	private Timestamp updatedAt;
+	private String updatedBy;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	
+	
+}
